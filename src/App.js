e1af22f14d36782b426ce3f3cc5d3ae1822d4fc1
Sumair Hassan <86984915+sumairhw@users.noreply.github.com>
2022-07-14 15:44:50 +0500

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Search = ({ onChange, searchTerm }) => {
+  console.log("search renders");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="search">Search : </label>
+      <input id="search" type="text" onChange={onChange} value={searchTerm} />
     </div>
   );
-}
+};
+
+const List = ({ list }) => {
+  console.log("list renders", list);
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} {...item} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = ({ title, url, author, num_comments, points }) => (
+  <li>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </li>
+);
+
+const App = () => {
+  console.log("app renders");
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search onChange={handleChange} />
+      <hr />
+      <List list={searchedStories} searchTerm={searchTerm} />
+    </div>
+  );
+};
 
 export default App;
