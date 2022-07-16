b0578efc409974ad6b26810a2dd6f4505f42fd5d
Sumair Hassan <86984915+sumairhw@users.noreply.github.com>
2022-07-16 14:59:14 +0500

    added css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import axios from 'axios'
+import './App.css'
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialState)
@@ -9,6 +10,7 @@ const useSemiPersistentState = (key, initialState) => {
 
   return [value, setValue]
 }
+
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -86,8 +88,8 @@ const App = () => {
     })
   }
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
@@ -115,15 +117,20 @@ const List = ({ list, onRemoveItem }) => {
 }
 
 const Item = ({ item, onRemoveItem }) => (
-  <li>
-    <span>
+  <li className="item">
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button onClick={() => onRemoveItem(item)}>Dismiss</button>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
+        className="button button_small"
+        onClick={() => onRemoveItem(item)}
+      >
+        Dismiss
+      </button>
     </span>
   </li>
 )
@@ -144,20 +151,23 @@ const InputWithLabel = ({
   }, [isFocused])
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
       <input
         id={id}
         type={type}
         value={value}
         onChange={onInputChange}
         ref={inputRef}
+        className="input"
       />
     </>
   )
 }
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -166,7 +176,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
